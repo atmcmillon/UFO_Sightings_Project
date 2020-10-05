90bd43f8910942b263e8e20a8b5a2c499c3f3dd0
atmcmillon <56739546+atmcmillon@users.noreply.github.com>
2020-10-05 15:29:10 -0500

    Update ufosightingsus.py

--- ufosightingsus.py
+++ ufosightingsus.py
@@ -5,9 +5,10 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import sweetviz as sv
+import matplotlib as plt
 #import geopandas as gp
 
-ufodf = pd.read_csv('scrubbed.csv', index_col=0, low_memory=False, parse_dates=True)
+ufodf = pd.read_csv('scrubbed.csv', index_col=0, low_memory=False)
 ufodf = ufodf.reset_index()
 ufodfinfo = ufodf.info
 ufodfshape = ufodf.shape
@@ -15,11 +16,18 @@ ufodfshape = ufodf.shape
 
 """ufo_report = sv.analyze(ufodf)
 ufo_report.show_html('scrubbed.html')"""
+#Undo lines 17 and 18 for the sweetviz data visualization file to view locally in a browser window
 
-#ufodf['datetime'] = ufodf['datetime'].dt.strftime('%d-%m-%Y %H:%M')
-#Why are you being difficult about converting to dt?
+
 date_sighting = ufodf['datetime']
+
+date_sighting = pd.to_datetime(ufodf['datetime'], errors='coerce')
+
 date_logged = ufodf['date posted']
+
+date_logged = pd.to_datetime(ufodf['date posted'], errors='coerce')
+
+pd.to_datetime(date_logged)
 city = ufodf['city'].str.capitalize()
 state = ufodf['state'].str.upper()
 country = ufodf['country'].str.upper()
@@ -27,7 +35,6 @@ shape = ufodf['shape'].str.capitalize()
 #lat = ufodf['latitude']
 #lon = ufodf['longitude']
 
-"""uniqueco = country.unique()
+uniqueco = country.unique()
 uniquesh = shape.unique()
-
-print(uniquesh)"""
+uniquest = state.unique()
